e892a484655cf37222885d44ac698c160274645e
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2024-08-05 21:03:16 +0530

    file changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <><h1 className="text-3xl font-bold underline">Test</h1></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Test</h1>
+    </>
+  );
 }
 
 export default App;
